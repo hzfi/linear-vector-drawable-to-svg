61d9cfbcf97ec0898a865a4680657021513ae806
hzfi <hzfi@hx.fyi>
2025-10-11 22:28:30 +0800

    update ns

--- bin/lvd2svg.mjs
+++ bin/lvd2svg.mjs
@@ -101,26 +101,26 @@ const gradient2def = (name, json) => {
 
     const stopArr = item.map(obj => {
       const { $: x } = obj;
-      const offset = x['android:offset']
-      const { c: color, alpha } = color2c(x['android:color'])
+      const offset = x[GLOB_CONFIG.ns + ':offset']
+      const { c: color, alpha } = color2c(x[GLOB_CONFIG.ns + ':color'])
       if (!color) return ''
       return `<stop stop-color="${color}"  stop-opacity="${alpha}"  offset="${Number(offset)}"/>`
     })
 
-    if (meta['android:type'] === 'linear') {
-      const x1 = Number(meta['android:startX'])
-      const x2 = Number(meta['android:endX'])
-      const y1 = Number(meta['android:startY'])
-      const y2 = Number(meta['android:endY'])
+    if (meta[GLOB_CONFIG.ns + ':type'] === 'linear') {
+      const x1 = Number(meta[GLOB_CONFIG.ns + ':startX'])
+      const x2 = Number(meta[GLOB_CONFIG.ns + ':endX'])
+      const y1 = Number(meta[GLOB_CONFIG.ns + ':startY'])
+      const y2 = Number(meta[GLOB_CONFIG.ns + ':endY'])
       return `<linearGradient id="${name}" x1="${x1}" x2="${x2}" y1="${y1}" y2="${y2}" >
 ${stopArr.join('\n')}
 </linearGradient>`
 
     }
-    if (meta['android:type'] === 'radial') {
-      const cx = Number(meta['android:centerX'])
-      const cy = Number(meta['android:centerY'])
-      const r = Number(meta['android:gradientRadius'])
+    if (meta[GLOB_CONFIG.ns + ':type'] === 'radial') {
+      const cx = Number(meta[GLOB_CONFIG.ns + ':centerX'])
+      const cy = Number(meta[GLOB_CONFIG.ns + ':centerY'])
+      const r = Number(meta[GLOB_CONFIG.ns + ':gradientRadius'])
       return `<radialGradient id="${name}" cx="${cx}" cy="${cy}" r="${r}" >
 ${stopArr.join('\n')}
 </radialGradient>`
@@ -129,13 +129,36 @@ ${stopArr.join('\n')}
   }
 }
 
+const hasColor = (val) => {
+  const key = val?.split('/').at(-1).replace('$', '')
+  const colorDef = colorContainer.get(key)
+  if (!colorDef) {
+    return { key }
+  }
+  return { key, colorDef }
+}
+
+const getMajoritykey = (arr) => {
+  const countMap = arr.reduce((p, c) => { p.has(c) ? p.set(c, p.get(c) + 1) : p.set(c, 1); return p }, new Map())
+  const key = [...countMap].map(([a, b]) => [b, a])[0][1]
+  return key
+
+}
+
+
 const v2svg = (json) => {
   const vector = json.vector
   if (vector) {
-    // console.log('vector', vector);
+    console.log('vector', vector);
+
 
     const { $: meta } = vector;
+    const metaKeyArr = Object.keys(meta).map(x => x.split(':')[0])
+    const globalKey = getMajoritykey(metaKeyArr)
+    GLOB_CONFIG.ns = globalKey;
 
+    console.log('GLOB_CONFIG.ns', GLOB_CONFIG );
+    
 
     const v2str = (v, lev = 0) => {
       const { path, group } = v;
@@ -143,41 +166,50 @@ const v2svg = (json) => {
       let content = ''
       path?.forEach((obj) => {
         const { $: x } = obj;
+
         const attr = {
-          d: x['android:pathData']
+          d: x[GLOB_CONFIG.ns + ':pathData']
         };
 
-        if (x['android:name']) {
-          attr.id = x['android:name']
+        console.log('================', attr );
+        
+        if (x[GLOB_CONFIG.ns + ':name']) {
+          attr.id = x[GLOB_CONFIG.ns + ':name']
         }
-        if (x['android:width']) {
-          attr.width = Number(x['android:width'])
+        if (x[GLOB_CONFIG.ns + ':width']) {
+          attr.width = Number(x[GLOB_CONFIG.ns + ':width'])
         }
-        if (x['android:width']) {
-          attr.width = Number(x['android:width'])
+        if (x[GLOB_CONFIG.ns + ':width']) {
+          attr.width = Number(x[GLOB_CONFIG.ns + ':width'])
         }
 
-        if (x['android:strokeColor']?.startsWith('@')) {
-          const key = x['android:strokeColor']?.split('/').at(-1).replace('$', '')
-          def += '\n' + colorContainer.get(key) || ''
-          attr.stroke = `url(#${key})`
-          attr.fill = "none"
-        } else if (x['android:strokeColor']) {
-          const { c, alpha } = color2c(x['android:strokeColor'])
+        if (x[GLOB_CONFIG.ns + ':strokeColor']?.startsWith('@')) {
+          const { key, colorDef } = hasColor(x[GLOB_CONFIG.ns + ':strokeColor'])
+          if (colorDef) {
+            def += '\n' + colorDef
+            attr.stroke = `url(#${key})`
+            attr.fill = "none"
+          } else {
+            attr.stroke = key
+          }
+        } else if (x[GLOB_CONFIG.ns + ':strokeColor']) {
+          const { c, alpha } = color2c(x[GLOB_CONFIG.ns + ':strokeColor'])
           if (c) {
             attr.stroke = c
             attr['stroke-opacity'] = alpha
             attr.fill = "none"
           }
         }
-
-
-        if (x['android:fillColor']?.startsWith('@')) {
-          const key = x['android:fillColor']?.split('/').at(-1).replace('$', '')
-          def += colorContainer.get(key) || ''
-          attr.fill = `url(#${key})`
-        } else if (x['android:fillColor']) {
-          const { c, alpha } = color2c(x['android:fillColor'])
+        if (x[GLOB_CONFIG.ns + ':fillColor']?.startsWith('@')) {
+          const { key, colorDef } = hasColor(x[GLOB_CONFIG.ns + ':fillColor'])
+          if (colorDef) {
+            def += '\n' + colorDef
+            attr.fill = `url(#${key})`
+          } else {
+            attr.fill = key
+          }
+        } else if (x[GLOB_CONFIG.ns + ':fillColor']) {
+          const { c, alpha } = color2c(x[GLOB_CONFIG.ns + ':fillColor'])
           if (c) {
             attr.fill = c
             attr['fill-opacity'] = alpha
@@ -185,29 +217,29 @@ const v2svg = (json) => {
         }
 
 
-        if (x['android:strokeWidth']) {
-          attr['stroke-width'] = x['android:strokeWidth']
+        if (x[GLOB_CONFIG.ns + ':strokeWidth']) {
+          attr['stroke-width'] = x[GLOB_CONFIG.ns + ':strokeWidth']
         }
-        if (x['android:strokeAlpha']) {
-          attr['stroke-opacity'] = x['android:strokeAlpha']
+        if (x[GLOB_CONFIG.ns + ':strokeAlpha']) {
+          attr['stroke-opacity'] = x[GLOB_CONFIG.ns + ':strokeAlpha']
         }
-        if (x['android:fillAlpha']) {
-          attr['fill-opacity'] = Number(x['android:fillAlpha'])
-        }
-
-        if (x['android:strokeLineCap']) {
-          attr['stroke-linecap'] = x['android:strokeLineCap']
-        }
-        if (x['android:strokeLineJoin']) {
-          attr['stroke-linejoin'] = x['android:strokeLineJoin']
-        }
-        if (x['android:strokeMiterLimit']) {
-          attr['stroke-miterlimit'] = Number(x['android:strokeMiterLimit'])
+        if (x[GLOB_CONFIG.ns + ':fillAlpha']) {
+          attr['fill-opacity'] = Number(x[GLOB_CONFIG.ns + ':fillAlpha'])
         }
 
+        if (x[GLOB_CONFIG.ns + ':strokeLineCap']) {
+          attr['stroke-linecap'] = x[GLOB_CONFIG.ns + ':strokeLineCap']
+        }
+        if (x[GLOB_CONFIG.ns + ':strokeLineJoin']) {
+          attr['stroke-linejoin'] = x[GLOB_CONFIG.ns + ':strokeLineJoin']
+        }
+        if (x[GLOB_CONFIG.ns + ':strokeMiterLimit']) {
+          attr['stroke-miterlimit'] = Number(x[GLOB_CONFIG.ns + ':strokeMiterLimit'])
+        }
 
-        if (x['android:fillType']) {
-          attr['fill-rule'] = x['android:fillType']?.toLowerCase()
+
+        if (x[GLOB_CONFIG.ns + ':fillType']) {
+          attr['fill-rule'] = x[GLOB_CONFIG.ns + ':fillType']?.toLowerCase()
         }
 
         const attrStr = Object.entries(attr).map(([k, v]) => `${k}="${v}"`).join(' ')
@@ -219,7 +251,7 @@ const v2svg = (json) => {
           if (x['clip-path']) {
             x['clip-path']?.forEach((y, j) => {
               def += `\n<clipPath id="_clippath_${lev}_${i}_${j}">
-<path d="${y['$']['android:pathData']}"/>
+<path d="${y['$'][GLOB_CONFIG.ns + ':pathData']}"/>
 </clipPath>`
               attr += `clip-path="url(#_clippath_${lev}_${i}_${j})"`
             })
@@ -238,8 +270,8 @@ const v2svg = (json) => {
     const { def, content } = v2str(vector)
 
     const defs = def ? `<defs>${def} </defs>` : ''
-    const w = Number(meta['android:viewportWidth'])
-    const h = Number(meta['android:viewportHeight'])
+    const w = Number(meta[GLOB_CONFIG.ns + ':viewportWidth'])
+    const h = Number(meta[GLOB_CONFIG.ns + ':viewportHeight'])
     return `<?xml version="1.0" encoding="UTF-8"?>
 <svg viewBox="0 0 ${w} ${h}" xmlns="http://www.w3.org/2000/svg">
 ${defs}${content}
@@ -251,6 +283,9 @@ ${defs}${content}
 const baseDir = './'
 let outDir = './out'
 
+const GLOB_CONFIG = {
+  ns: 'android'
+}
 const main = () => {
   if (checkArgs()) {
     return
